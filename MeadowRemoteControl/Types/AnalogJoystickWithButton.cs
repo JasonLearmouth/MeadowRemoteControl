188e399e2dcb9b465d50e860c3a9e70c7e6125a2
Jason Learmouth <jason.learmouth@essist.com.au>
2021-04-23 21:36:16 +1000

    Added Joysticks to dataPackage
    Added AnalogJoystickWithButton class.
    Added properties for horizontal, vertical, and button for both joysticks to DataPackage struct
    Populate Joystick values in BuildDataPackage

---
+++ MeadowRemoteControl/Types/AnalogJoystickWithButton.cs
@@ -0,0 +1,33 @@
+﻿using Meadow.Foundation.Sensors.Buttons;
+using Meadow.Foundation.Sensors.Hid;
+using Meadow.Hardware;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Meadow.Foundation.Sensors.Hid.AnalogJoystick;
+
+namespace MeadowRemoteControl.Types
+{
+    public class AnalogJoystickWithPushButton
+    {
+        public AnalogJoystickWithPushButton(IIODevice device, IPin horizontalPin, IPin verticalPin, IPin buttonInputPin, JoystickCalibration calibration = null, bool isInverted = false, ResistorMode buttonResistorMode = ResistorMode.InternalPullUp)
+        {
+            Joystick = new AnalogJoystick(device.CreateAnalogInputPort(horizontalPin), device.CreateAnalogInputPort(horizontalPin), calibration, isInverted);
+            Button = new PushButton(device, buttonInputPin, buttonResistorMode);
+        }
+
+        public AnalogJoystick Joystick { get; private set; }
+
+        public PushButton Button { get; private set; }
+
+        // Joystick returns a float from 0.0 - 1.0
+        // We convert to a single byte 0 -> 254
+        public uint HorizontalValue => (uint)Joystick.GetHorizontalValue().Result * 254;
+        public uint VerticalValue => (uint)Joystick.GetVerticalValue().Result * 254;
+
+        public uint ButtonState => Button.State ? (uint)1 : 0;
+        
+    }
+}
